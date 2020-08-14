9f0d2a074ef936ab626a47d5e8f99d073a7bb7c6
dbaldzhiev <dbaldzhiev@gmail.com>
2020-08-14 14:18:31 +0300

    minor cosmetic stuff

--- cadutils.py
+++ cadutils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-### VERSION 2.00 STANDALONE
-### TESTED ON WINDOWS PYTHON 3.8.2
+# VERSION 2.00 STANDALONE
+# TESTED ON WINDOWS PYTHON 3.8.2
 
 import logging
 import re
@@ -20,6 +20,7 @@ class ReadCadastralFile:
         self.Header = HeaderLayer(data)
         self.CadasterLayer = CadasterLayer(data, self.Header)
         self.CadasterControl = ControlCadastre(data)
+
         def controlCheck(cl, cc):
             if (len(cl.lineObj) == cc.lines) and (len(cl.contourObj) == cc.contours) and (
                     len(cl.symbolObj) == cc.symb) and (len(cl.gepointObj) == cc.points) and (len(cl.textObj) == cc.txt):
@@ -38,7 +39,7 @@ class HeaderLayer:
 
     def __init__(self, data):
         rx_header = re.compile(
-            "HEADER\s*VERSION\s+(\S+)\s*EKATTE\s+(.*)\s*NAME\s+(.*)\s*PROGRAM\s+(\S+)\s+V?(\S+)\s*DATE\s+(.*)\s*FIRM\s+(.*)\s*REFERENCE\s+(\S+)\s+(\S+)\s*WINDOW\s+(.*)\s*COORDTYPE\s+(\d+),(.*)\s*CONTENTS\s+(.*)\s*COMMENT\s+(.*)\s*END_HEADER",
+            r"HEADER\s*VERSION\s+(\S+)\s*EKATTE\s+(.*)\s*NAME\s+(.*)\s*PROGRAM\s+(\S+)\s+V?(\S+)\s*DATE\s+(.*)\s*FIRM\s+(.*)\s*REFERENCE\s+(\S+)\s+(\S+)\s*WINDOW\s+(.*)\s*COORDTYPE\s+(\d+),(.*)\s*CONTENTS\s+(.*)\s*COMMENT\s+(.*)\s*END_HEADER",
             re.MULTILINE)
         extract = rx_header.search(data).groups()
 
@@ -64,19 +65,19 @@ class HeaderLayer:
 
 def objectSearch(data):
     rx_textobj = re.compile(
-        "^T\s+(\d+)\s+(\d+)\s+(\S+)\s+(\S+)\s+(\S+)\s(\S+)\s+(\S+)\s+(\S+)\s+[LCR][TCD]\s+(?:(?:\"(.*)\"\s)?[CPLVSA]\s+(\S+)\s(AN|SI|NU|LE|XC|YC|HI|AR|LP|AD|ST|IO)\s)?\"(.*)\"",
+        r"^T\s+(\d+)\s+(\d+)\s+(\S+)\s+(\S+)\s+(\S+)\s(\S+)\s+(\S+)\s+(\S+)\s+([LCR])([TCD])\s+(?:(?:\"(.*)\"\s)?([CPLVSA])\s+(\S+)\s(AN|SI|NU|LE|XC|YC|HI|AR|LP|AD|ST|IO)\s)?\"(.*)\"",
         re.MULTILINE)
     rx_lineobj = re.compile(
-        "(?:^L\s+(\d+)\s+(\d+)\s+(\d+)\s+((?:\d{1,2}\.\d{1,2}\.\d{2,4})|(?:0))\s+((?:\d{1,2}\.\d{1,2}\.\d{2,4})|(?:0))\s+)((?:(?:\S+)\s+(?:\S+)\s+(?:\S+)\s+(?:\S+)\s+(?:\S+)\s+(?:\S+);(?:\s+)?)*)",
+        r"(?:^L\s+(\d+)\s+(\d+)\s+(\d+)\s+((?:\d{1,2}\.\d{1,2}\.\d{2,4})|(?:0))\s+((?:\d{1,2}\.\d{1,2}\.\d{2,4})|(?:0))\s+)((?:(?:\S+)\s+(?:\S+)\s+(?:\S+)\s+(?:\S+)\s+(?:\S+)\s+(?:\S+);(?:\s+)?)*)",
         re.MULTILINE)
     rx_conobj = re.compile(
-        "C\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+((?:\d{1,2}\.\d{1,2}\.\d{2,4})|(?:0))\s+((?:\d{1,2}\.\d{1,2}\.\d{2,4})|(?:0))\s+((?:(?:\d+)\s+)*)",
+        r"C\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+((?:\d{1,2}\.\d{1,2}\.\d{2,4})|(?:0))\s+((?:\d{1,2}\.\d{1,2}\.\d{2,4})|(?:0))\s+((?:(?:\d+)\s+)*)",
         re.MULTILINE)
     rx_symbobj = re.compile(
-        "S\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+((?:\d{1,2}\.\d{1,2}\.\d{2,4})|(?:0))\s+((?:\d{1,2}\.\d{1,2}\.\d{2,4})|(?:0))",
+        r"S\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+((?:\d{1,2}\.\d{1,2}\.\d{2,4})|(?:0))\s+((?:\d{1,2}\.\d{1,2}\.\d{2,4})|(?:0))",
         re.MULTILINE)
     rx_geopointobj = re.compile(
-        "P\s+(\d+)\s+(\d+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\d+)\s+(?:(\d+(?:.\d+)*)|(?:0))\s+(?:(\d+(?:.\d+)*)|(?:0))\s+(\d+)\s+(?:(\d+(?:.\d+)*)|(?:0))\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+\"(.*)\"\s+((?:\d{1,2}\.\d{1,2}\.\d{2,4})|(?:0))\s+((?:\d{1,2}\.\d{1,2}\.\d{2,4})|(?:0))",
+        r"P\s+(\d+)\s+(\d+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\d+)\s+(?:(\d+(?:.\d+)*)|(?:0))\s+(?:(\d+(?:.\d+)*)|(?:0))\s+(\d+)\s+(?:(\d+(?:.\d+)*)|(?:0))\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+\"(.*)\"\s+((?:\d{1,2}\.\d{1,2}\.\d{2,4})|(?:0))\s+((?:\d{1,2}\.\d{1,2}\.\d{2,4})|(?:0))",
         re.MULTILINE)
     lineObjectsStrings = rx_lineobj.finditer(data)
     contObjectsStrings = rx_conobj.finditer(data)
@@ -89,7 +90,7 @@ def objectSearch(data):
 class CadasterLayer:
     def __init__(self, data, hdr):
 
-        rx_cadLayer = re.compile("LAYER CADASTER([\s\S]*?)END_LAYER")
+        rx_cadLayer = re.compile(r"LAYER CADASTER([\s\S]*?)END_LAYER")
         extract = rx_cadLayer.search(data).group()
         parse = objectSearch(extract)
 
@@ -104,14 +105,13 @@ class CadasterLayer:
         if parse[4]:
             self.symbolObj = [SymbolC(symb.groups(), hdr) for symb in parse[4]]
 
-
     def __getitem__(self, item):
         return getattr(self, item)
 
 
 class ControlCadastre:
     def __init__(self, data):
-        rx_controlCadaster = re.compile("CONTROL CADASTER([\S\s]*)END_CONTROL")
+        rx_controlCadaster = re.compile(r"CONTROL CADASTER([\S\s]*)END_CONTROL")
 
         extract = rx_controlCadaster.search(data).group()
 
@@ -128,8 +128,8 @@ class ControlCadastre:
 class Buildings:
     def __init__(self, data, hdr):
 
-        rx_LevelSchemasLayer = re.compile("LAYER SHEMI([\s\S]*?)END_LAYER")
-        rx_Levels = re.compile("ET\s+(\S+)\s+(\S+)\s+([\s\S]*?)END_ET")
+        rx_LevelSchemasLayer = re.compile(r"LAYER SHEMI([\s\S]*?)END_LAYER")
+        rx_Levels = re.compile(r"ET\s+(\S+)\s+(\S+)\s+([\s\S]*?)END_ET")
         extract = rx_Levels.finditer(rx_LevelSchemasLayer.search(data).group(0))
         self.list = []
         if extract:
@@ -152,8 +152,7 @@ class Building:
         self.levels = []
 
     def addLevel(self, lvldata, hdr):
-        l = LevelObj(lvldata, hdr)
-        self.levels.append(l)
+        self.levels.append(LevelObj(lvldata, hdr))
 
     def __getitem__(self, item):
         return getattr(self, item)
@@ -180,7 +179,7 @@ class LineC:
         self.bordertype = str(array[2])
         self.datecreated = str(array[3])
         self.datedestroyed = str(array[4])
-        rx_ptlist = re.compile("(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+);")
+        rx_ptlist = re.compile(r"(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+);")
         self.ptlist = [LinecPt(p.groups(), hdr) for p in rx_ptlist.finditer(array[5])]
         self.get_point_sequence = [pt.get_XY for pt in self.ptlist]
         self.get_referenced_point_sequence = [pt.get_XYR for pt in self.ptlist]
@@ -214,13 +213,14 @@ class ContC:
         self.posX = float(array[3])
         self.datecreated = array[4]
         self.datedestroyed = array[5]
-        rx_contid = re.compile("(\d+)")
+        rx_contid = re.compile(r"(\d+)")
         self.ids = rx_contid.findall(array[6])
         self.posXR = self.posX + hdr.refX
         self.posYR = self.posY + hdr.refY
 
     def __getitem__(self, item):
         return getattr(self, item)
+
 
 class TextC:
     def __init__(self, array, hdr):
@@ -276,6 +276,7 @@ class GeoPointC:
     def __getitem__(self, item):
         return getattr(self, item)
 
+
 class SymbolC:
     def __init__(self, array, hdr):
         self.type = array[0]
@@ -292,6 +293,7 @@ class SymbolC:
     def __getitem__(self, item):
         return getattr(self, item)
 
+
 def opener(filename):
     if path.exists(filename[:-4] + "_cached.tcad"):
         filename = filename[:-4] + "_cached.tcad"
@@ -299,7 +301,7 @@ def opener(filename):
     filetext = f.read()
     try:
         cadText = filetext.decode("utf-8")
-    except:
+    except Exception:
         cadText = translate(filetext).replace("\r", "")
         ff = open(filename[:-4] + "_cached.tcad", "x", encoding="utf-8")
         ff.write(cadText)
@@ -311,7 +313,7 @@ def translate(textbytes):
     goodText = ""
     print("TRANSLATION IN PROGRESS")
     for i in tqdm(range(len(textbytes))):
-        if ((textbytes[i] >= 128) and (textbytes[i] <= 191)):
+        if (textbytes[i] >= 128) and (textbytes[i] <= 191):
             letter = str(mik.mikdict.get(textbytes[i]))
         else:
             letter = chr(textbytes[i])
